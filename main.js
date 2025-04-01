8aa763d8124e3b2bff6cce80331a051e1f697787
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-04-01 12:51:24 -0300

    Aplicando eventos e callbacks

--- main.js
+++ main.js
@@ -1,26 +1,35 @@
-const randomNumber = Math.round(Math.random() * 10);
-let xAttempts = 1;
+const screen1 = document.querySelector('.screen1')
+const screen2 = document.querySelector('.screen2')
+const randomNumber = Math.round(Math.random() * 10)
+let xAttempts = 1
 
-function handleClick(event) {
-  event.preventDefault();
+// função callback
+function handleTryClick(event) {
+  event.preventDefault()
 
-  const inputNumber = document.querySelector('#inputNumber');
+  const inputNumber = document.querySelector('#inputNumber')
 
-  console.log(randomNumber);
+  if (Number(inputNumber.value) == randomNumber) {
+    screen1.classList.add('hide')
+    screen2.classList.remove('hide')
 
-  if(Number(inputNumber.value) < 0) {
-    alert('Por favor, digite um número positivo.');
+    document.querySelector(
+      '.screen2 h2'
+    ).innerText = `acertou em ${xAttempts} tentativas`
   }
 
-  if(Number(inputNumber.value) == randomNumber) {
-    document.querySelector(".screen1").classList.add("hide");
-    document.querySelector(".screen2").classList.remove("hide");
-
-    document
-      .querySelector(".screen2 h2")
-      .innerText = `acertou em ${xAttempts} tentativas`;
-  }
-
-  xAttempts++;
-
+  inputNumber.value = ''
+  xAttempts++
 }
+
+// Eventos
+const btnTry = document.querySelector('#btnTry')
+const btnReset = document.querySelector('#btnReset')
+
+btnTry.addEventListener('click', handleTryClick);
+
+btnReset.addEventListener('click', function () {
+  screen1.classList.remove('hide')
+  screen2.classList.add('hide')
+  xAttempts = 1
+});
